1dc65a3654d372a575506dd0b531c868910a9c89
demonkillerr <gaurangvishwakarma619@gmail.com>
2024-10-14 00:53:37 +0100

    add remove cpu_microcode

--- src/modules/packages/main.py
+++ src/modules/packages/main.py
@@ -10,125 +10,26 @@ import libcalamares
 # 2) add functions for Packagechooser
 # 3) add and sanitize variables for cpu_type and and write a function to remove specific microcode on the basis of cpu_type
 
+def get_cpu_type():
+    """Get the CPU type (Intel or AMD)."""
+    cpu_info = {}
+    with open('/proc/cpuinfo', 'r') as cpuinfo_file:
+        for line in cpuinfo_file:
+            if line.startswith('vendor_id'):
+                cpu_info['vendor'] = line.split(':')[1].strip()
+                break
+    return cpu_info.get('vendor', 'Unknown')
+
 
 def run():
-    """ Package removal module. Live only packages, surplus language packs. """
+    cpu_type = get_cpu_type()
+    if 'GenuineIntel' in cpu_type:
+        print("Intel CPU detected... removing amd microcode")
+        libcalamares.utils.target_env_call(['pacman', '-Rns', '--noconfirm', 'amd-ucode'])
 
-    install_path = libcalamares.globalstorage.value("rootMountPoint")
-    fw_type = libcalamares.globalstorage.value("firmwareType")
+    elif 'AuthenticAMD' in cpu_type:
+        print("AMD CPU detected... removing intel microcode")
+        libcalamares.utils.target_env_call(['pacman', '-Rns', '--noconfirm', 'intel-ucode'])
+    else:
+        print("Unknown CPU type")
 
-    # remove any db.lck
-    db_lock = os.path.join(install_path, "var/lib/pacman/db.lck")
-    if os.path.exists(db_lock):
-        with misc.raised_privileges():
-            os.remove(db_lock)
-
-    # Remove Calamares and depends
-    if os.path.exists("{!s}/usr/bin/calamares" .format(install_path)):
-        print('Removing installer packages')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rns', '--noconfirm', 'calamares', 'calamares-debug', 'console-setup'])
-
-    # Remove welcome
-    if os.path.exists("{!s}/usr/bin/welcome" .format(install_path)):
-        print('Removing live ISO packages')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rns', '--noconfirm', 'welcome'])
-
-    # Remove hardware detection
-    if os.path.exists("{!s}/etc/kdeos-hwdetect.conf" .format(install_path)):
-        print('Removing live start-up packages')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rns', '--noconfirm', 'hardware-detection'])
-
-    # Remove init-live
-    if os.path.exists("{!s}/etc/live" .format(install_path)):
-        print('Removing live configuration packages')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-R', '--noconfirm', 'init-live'])
-
-    # Remove surplus bootloader packages
-    if fw_type == 'efi':
-        print('Removing grub packages')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rncs', '--noconfirm', 'grub'])
-
-    if fw_type == 'bios':
-        print('Removing EFI packages')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rns', '--noconfirm', 'efibootmgr', 'refind-efi'])
-
-    # Remove VirtualBox Guest packages
-    if os.path.exists("{!s}/usr/bin/VBoxClient" .format(install_path)):
-        if 'hypervisor' in open('/proc/cpuinfo').read():
-            print('Virtual Machine')
-            vbox = subprocess.check_output('systemd-detect-virt')
-            if 'oracle' in str(vbox):
-                print('VirtualBox')
-            else:
-                print('Removing guest-utils')
-                libcalamares.utils.target_env_call(
-                    ['pacman', '-R', '--noconfirm', 'virtualbox-guest-utils'])
-        else:
-            print('Removing guest-utils')
-            libcalamares.utils.target_env_call(
-                ['pacman', '-R', '--noconfirm', 'virtualbox-guest-utils'])
-
-    # Packagechooser outcome / remove LibreOffice l10n
-    packages = libcalamares.globalstorage.value("packagechooser_packagechooserq")
-
-    if packages == 'no_office_suite':
-        print('Removing LibreOffice and Calligra')
-        libcalamares.utils.target_env_call(
-                ['pacman', '-Rns', '--noconfirm', 'libreoffice', 'calligra'])
-
-    if packages == 'calligra':
-        print('Calligra selected')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rns', '--noconfirm', 'libreoffice'])
-
-    if packages == 'libreoffice':
-        print('LibreOffice selected')
-        libcalamares.utils.target_env_call(
-            ['pacman', '-Rns', '--noconfirm', 'calligra'])
-
-    if packages == 'minimal_install':
-        print('Minimal Install')
-        pkgs = ['plasma-workspace-wallpapers',
-                'partitionmanager',
-                'gwenview',
-                'spectacle',
-                'sweeper',
-                'plasma-firewall',
-                'quassel',
-                'smplayer',
-                'haruna',
-                'oxygen-sounds',
-                'elisa',
-                'kde-gtk-config',
-                'kcp',
-                'pacmanlogviewer',
-                'seafile-client',
-                'kamoso',
-                'kget',
-                'kompare',
-                'simplescreenrecorder',
-                'power-profiles-daemon',
-                'k3b',
-                'system-config-printer',
-                'print-manager',
-                'hplip',
-                'epson-inkjet-printer-escpr',
-                'pyqt5-python3',
-                'kpat',
-                'neochat',
-                'libreoffice',
-                'calligra']
-        for pkg in pkgs:
-            try:
-                libcalamares.utils.target_env_call(
-                    ['pacman', '-Rns', '--noconfirm', '{!s}' .format(pkg)])
-            except:
-                print("Could not remove package " + pkg)
-
-    print('package removal completed')
