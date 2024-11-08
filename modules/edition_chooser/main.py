2368d1d922b6628cca589b0a066065050e52b60b
demonkillerr <gaurangvishwakarma619@gmail.com>
2024-11-08 04:17:04 +0000

    add scaffold functions

--- modules/edition_chooser/main.py
+++ modules/edition_chooser/main.py
@@ -3,3 +3,14 @@
 import os
 import subprocess
 import libcalamares
+
+# might have to patch package chooser here
+
+def get_edition_version():
+    pass
+
+def desktop_version():
+    pass
+
+def set_system_theme():
+    pass
