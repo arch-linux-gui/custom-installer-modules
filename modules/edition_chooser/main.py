32ae48c1841345351f148b86c4d927116e6f977d
demonkillerr <gaurangvishwakarma619@gmail.com>
2024-11-16 00:58:43 +0000

    add todo

--- modules/edition_chooser/main.py
+++ modules/edition_chooser/main.py
@@ -4,7 +4,13 @@ import os
 import subprocess
 import libcalamares
 
-# might have to patch package chooser here
+#NOTE: This module uses packagechooser as it's frontend. It also receives GS values from it.
+
+# #TODO:
+# 1) Based on packagechooser GS value, pass items to add/remove in packages_remover
+# 2) Set env variables
+# 3) Copy/Delete config files based on chosen edition (get_edition_version())
+# 4) Set configs (set_system_theme())
 
 def get_edition_version():
     pass
